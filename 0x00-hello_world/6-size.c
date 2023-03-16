2aa4237950052720f3f737e1e6b9b7a2ccacbc17
wil-mus <wilfred_musyoka@yahoo.com>
2023-03-16 16:44:24 -0700

    print of computer size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,6 @@ printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 printf("Size of float: %lu byte(s)\n", sizeof(float));
+
 return (0);
 }
