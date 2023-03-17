4be87aefa1cce4c4be0bcea37d73d4487b60b650
wil-mus <wilfred_musyoka@yahoo.com>
2023-03-16 17:47:02 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc $CFILE -S -masm-intel
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry
+ *
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
