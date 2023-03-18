90ef8e96c3a7b38ac85dedf6de14a9c95131e335
wil-mus <wilfred_musyoka@yahoo.com>
2023-03-17 18:49:08 -0700

    comb 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,4 +7,31 @@
  */
 
 int main(void)
+{
+	int m;
+	int n;
+	int o;
 
+	for (m = 0 ; m < 10 ; m++)
+	{
+		for (n = 1 ; n < 10 ; n++)
+		{
+			for (o = 2 ; o < 10 ; o++)
+			{
+				if (m < n && n < o)
+				{
+					putchar(m + '0');
+					putchar(n + '0');
+					putchar(o + '0');
+					if (m + n + o != 24)
+					{
+						putchar('.');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
