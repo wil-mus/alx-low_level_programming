9b00d9bd6b2b3bd2b40c0be0c9a734d649c726dc
wil-mus <wilfred_musyoka@yahoo.com>
2023-03-21 11:39:40 -0700

    printing alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - printing alphabets
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+char a;
+
+for (a = 'a' ; a <= 'z' ; a++)
+	_putchar(a);
+_puthar('\n');
+return (0);
+}
