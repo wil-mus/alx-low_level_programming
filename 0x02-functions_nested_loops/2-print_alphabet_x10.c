185cfbbe3bcc77b0728311db927ab1bdf07344f5
wil-mus <wilfred_musyoka@yahoo.com>
2023-03-21 13:23:12 -0700

    printing 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10 - print alphabet
+ *
+ * Return: 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	char b;
+
+	for (a = 1 ; a <= 10 ; a++)
+	{
+		for (b = 'a' ; b <= 'z' ; b++)
+			_putchar(b);
+		_putchar('\n');
+	}
+}
