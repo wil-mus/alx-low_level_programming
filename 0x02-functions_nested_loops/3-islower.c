395df6e32e38384dbe9819d16e501752f546c642
wil-mus <wilfred_musyoka@yahoo.com>
2023-03-21 19:36:36 -0700

    add latest commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
- * islower - print lowercase
+ *_islower - print lowercase
  *
- * Return: 1 if low
+ * @c: paramater
+ * Return: 1 if lowercase otherwise 0
  */
 
 int _islower(int c)
 {
-
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
