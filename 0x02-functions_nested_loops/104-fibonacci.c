6617769e5332aca9dc5b96ac72bf689b5820eb5d
wil-mus <wilfred_musyoka@yahoo.com>
2023-03-22 15:35:50 -0700

    finobacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ *
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
+
+	printf("%lu", bef);
+	for (i = 1 ; 1 < 98 ; i++)
+	{
+		printf("%lu", aft);
+		aft += bef;
+		bef = aft - bef;
+	}
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
+	for (i = 98 ; i < 98 ; i--)
+	{
+		printf("%lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l)
+		aft1 = aftq + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
+	}
+	printf("\n");
+	return (0);
+}
