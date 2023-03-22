442a3fa72913abd595a57b68d69b4e155f93d35a
wil-mus <wilfred_musyoka@yahoo.com>
2023-03-22 14:17:26 -0700

    print n times in table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
+ *print_times_table - print n times in table
  *
+ * @n: paramater of function
  *
- *
+ *Return: 0
  */
 
 void print_times_table(int n)
@@ -21,7 +23,30 @@ void print_times_table(int n)
 				{
 					_putchar(k + '0');
 				}
+				else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchat((k / 10) + '0');
+					_putchar((k % 10)  + '0');
+				}
+				else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar((k % 100) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
+			_putchar("\n");
 		}
 	}
 }
