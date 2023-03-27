9128a68ec1add055d236ba7cb38a0e125d4e0571
wil-mus <wilfred_musyoka@yahoo.com>
2023-03-27 14:50:09 -0700

    latest commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,15 +11,11 @@
 void puts2(char *str)
 {
 	int a = 0;
-	int b;
 
 	while (str[a] != '\0')
 	{
-		for (b = 0; b < 10 && str[a] != '\0'; b++)
-		{
-			_putchar(str[a]);
-			a++;
-		}
+		_putchar(str[a]);
+		a += 2;
 	}
 	_putchar('\n');
 }
