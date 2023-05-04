5170e982c4d1447070558ff8418a537536122f5a
wil-mus <wilfred_musyoka@yahoo.com>
2023-05-04 15:11:04 -0700

    print binary no

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary represantation of a number
+ *
+ * @n: no to print binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	int s = 0;
+
+	while ((n >> s) > 0)
+	{
+		s++;
+	}
+	s--;
+	if (s < 0)
+	{
+		_putchar('0');
+	}
+	while (s >= 0)
+	{
+		if ((n >> s) & 1)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+		s--;
+	}
+}
